414b32f110a49b179b8558ff24f2147a7f7cb3cd
anthonysioux91@outlook.com <anthonysu91@gmail.com>
2023-12-05 18:18:32 +0800

    jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,17 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:2.3.6")
+    implementation("io.ktor:ktor-client-cio:2.3.6")
+    //implementation("io.ktor:ktor-client-logging:2.3.6")
+    implementation("org.slf4j:slf4j-simple:1.6.1")
+
     testImplementation(kotlin("test"))
 }
 
@@ -25,3 +31,4 @@ kotlin {
 application {
     mainClass.set("MainKt")
 }
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,60 @@
 import java.io.IOException
 import java.net.InetAddress
+
 import java.util.concurrent.TimeUnit
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
 
 
-val ipAddress = "192.168.2.1"
-val isPingSuccess = ping(ipAddress)
-var trueCounter = 0
+val ipAddress = "192.168.10.2"
+var isPingSuccess = false
+
 var falseCounter = 0
-fun main(args: Array<String>) {
+val client = HttpClient(CIO)
+suspend fun main(args: Array<String>) {
     println("Author: Anthony")
-    println("Version: 1.0")
-    println("请勿关闭此窗口，缩小即可。")
+    println("Version: 1.1")
+    println("Please don't close this windows.")
+
 
     do{
         ping(ipAddress)
+        isPingSuccess = ping(ipAddress)
         if(isPingSuccess)
         {
-            trueCounter ++
+
             TimeUnit.SECONDS.sleep(5);
-            println("主机连接正常，第 $trueCounter 次")
+            println("Connected to host")
+            println("Getting task from host")
+
+            val response: HttpResponse = client.get("http://192.168.10.2:8080/task")
+            val respondStr = response.bodyAsText()
+            println(respondStr)
+            if(respondStr =="shutdown"){
+                println("Start to shutdown")
+                try {
+                    val command = "shutdown.exe -p" // -r 表示重启，-t 0 表示无延迟立即重启
+                    val process = Runtime.getRuntime().exec(command)
+                    process.waitFor()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+            }
         }
         else{
             println("Ping $ipAddress: $isPingSuccess")
+            TimeUnit.SECONDS.sleep(4)
             falseCounter ++
-            println("尝试连接主机失败，第 $falseCounter 次")
+            println("failed to connect to host，the $falseCounter time")
             if(falseCounter >10)
             {
                 try {
-                    val command = "shutdown.exe -r -t 0" // -r 表示重启，-t 0 表示无延迟立即重启
+                    val command = "shutdown.exe -r -t 4" // -r 表示重启，-t 0 表示无延迟立即重启
                     val process = Runtime.getRuntime().exec(command)
                     process.waitFor()
                 } catch (e: IOException) {
@@ -37,7 +64,7 @@ fun main(args: Array<String>) {
                 }
             }
         }
-        println("请勿关闭此窗口，缩小即可。")
+        println("Please don't close this windows.")
     } while (true)
 }
 
@@ -45,7 +72,7 @@ fun main(args: Array<String>) {
 fun ping(ipAddress: String): Boolean {
     return try {
         val inet = InetAddress.getByName(ipAddress)
-        inet.isReachable(5000) // 设置超时时间为5秒
+        inet.isReachable(1000) // 设置超时时间为5秒
     } catch (ex: Exception) {
         false
     }
